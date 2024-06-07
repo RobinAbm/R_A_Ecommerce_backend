d63ca56ad1c09e0bcbc7ab2211fa50b004c717f3
robin-abraham-ta <robinabmrbn@gmail.com>
2024-06-07 14:06:12 +0530

    product history , adress

--- User/userschema.js
+++ User/userschema.js
@@ -2,27 +2,28 @@ const mongoose = require('mongoose')
 mongoose.connect(('mongodb://localhost:27017/Ecommerce'))
 
 var userchema = mongoose.Schema({
-  name:{
-    type:String,
-    required:true
+  name: {
+    type: String,
+    required: true
   },
-  number:{
-    type:Number,
-    required:true
+  number: {
+    type: Number,
+    required: true
   },
-  email:{
-    type:String,
-    required:true
+  email: {
+    type: String,
+    required: true
   },
-  password:{
-    type:String,
-    required:true
+  password: {
+    type: String,
+    required: true
   },
-  gender:{
-    type:String,
-    default:"male"
+  gender: {
+    type: String,
+    default: "male"
   },
-  image:Object
+  image: Object,
+ 
 })
 
 
